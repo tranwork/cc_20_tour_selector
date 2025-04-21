2b59d38fbebc17dcd1f7402a94821742792d294c
Jenny <work.trantran@gmail.com>
2025-04-20 21:38:06 -0400

    Add DestinationSelector component with unique name dropdown

--- src/components/DestinationSelector.jsx
+++ src/components/DestinationSelector.jsx
@@ -1,1 +1,24 @@
- 
+import React from 'react';
+
+function DestinationSelector({ tours, selected, setSelected }) {
+  const uniqueNames = ['All', ...new Set(tours.map((tour) => tour.name))];
+
+  return (
+    <div className="dropdown-container">
+      <label htmlFor="destination">Select a Destination: </label>
+      <select
+        id="destination"
+        value={selected}
+        onChange={(e) => setSelected(e.target.value)}
+      >
+        {uniqueNames.map((name) => (
+          <option key={name} value={name}>
+            {name}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+export default DestinationSelector;
